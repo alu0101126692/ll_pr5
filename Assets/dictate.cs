e015e5e54034588308fa8474461c158c2cbec55e
Pablo Torres <alu0101126692@ull.edu.es>
2021-11-19 14:43:39 +0000

    Añadido scripts principales

---
+++ Assets/dictate.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class dictate : MonoBehaviour
+{
+
+
+    private DictationRecognizer dictado;
+    public bool dictando;
+    public keyword reconocedorKeyword;
+
+    void Start()
+    {
+        dictado = new DictationRecognizer();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        administarDictado();
+    }
+
+
+    private void empezarDictado() {
+
+        dictado.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+        };
+
+        dictado.DictationHypothesis += (text) =>
+        {
+            Debug.LogFormat("Dictation hypothesis: {0}", text);
+        };
+
+        dictado.DictationComplete += (completionCause) =>
+        {
+            if (completionCause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+        };
+
+        dictado.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+    }
+    private void administarDictado() {
+        if (!dictando && !reconocedorKeyword.reconociendo) {
+            if (Input.GetKeyDown("g")) {
+                empezarDictado();
+                dictando = true;
+                //reconocedorKeyword.parar();
+                dictado.Start();
+            } 
+
+            
+        } else if (dictando){
+            if (Input.GetKeyDown("g")) {
+                dictando = false;
+                dictado.Stop();
+                parar();
+            } 
+        }
+    }
+
+    public void parar() {
+        if (dictado != null)
+            dictado.Dispose();
+    }
+}
---
+++ Assets/keyword.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class keyword : MonoBehaviour
+{
+    public string[] keywords;
+    private KeywordRecognizer recognizer;
+    public bool reconociendo;
+    public dictate reconocedorDictado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        recognizer = new KeywordRecognizer(keywords);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        administarReconocedor();
+        
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args) {
+        Debug.Log ("Keyword: " + args.text + "; Confidence: " + args.confidence + "; Start Time: " + args.phraseStartTime + "; Duration: "  + args.phraseDuration);
+    }
+
+
+    private void empezarKeyword() {
+
+        recognizer.OnPhraseRecognized += OnPhraseRecognized;
+    }
+    private void administarReconocedor() {
+        if (!reconociendo && !reconocedorDictado.dictando) {
+            if (Input.GetKeyDown("f")) {
+                empezarKeyword();
+                reconociendo = true;
+                //reconocedorDictado.parar();
+
+                recognizer.Start();
+            } 
+
+            
+        } else if (reconociendo){
+            if (Input.GetKeyDown("f")) {
+                reconociendo = false;
+                recognizer.Stop();
+                parar();
+            } 
+        }
+    }
+    
+    public void parar() {
+        recognizer.Dispose();
+    }
+}
